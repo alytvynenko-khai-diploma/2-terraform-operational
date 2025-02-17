256e9cf065fca6bb63b60c427df480761eb2e243
fecton <bopongamer@gmail.com>
2025-02-18 00:04:14 +0100

    feat(influxdb): deploy influxdb and telegraf

---
+++ 2-module-monitoring/3-module-influxdb-and-telegraf/telegraf.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "telegraf" {
+  name       = "telegraf"
+  repository = "https://helm.influxdata.com/"
+  chart      = "telegraf"
+  version = "1.8.55"
+  namespace = var.namespace
+
+  values     = [file("${path.module}/telegraf-values.yaml")]
+}
