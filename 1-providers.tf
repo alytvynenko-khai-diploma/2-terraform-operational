f92c82105581a294812e87ad0cdfef729c3a82bf
fecton <bopongamer@gmail.com>
2025-02-17 21:30:30 +0100

    feat: add helm provider

--- 1-providers.tf
+++ 1-providers.tf
@@ -6,10 +6,26 @@ terraform {
       source = "hashicorp/kubernetes"
       version = "2.35.1"
     }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.17.0"
+    }
   }
 }
 
+locals {
+  kubernetes_config_path = "~/.kube/config" # TODO: set up kubernetes provider
+  kubernetes_config_context = "docker-desktop" # TODO: set up kubernetes provider
+}
+
 provider "kubernetes" {
-  config_path    = "~/.kube/config" # TODO: set up kubernetes provider
-  config_context = "my-context"     # TODO: set up kubernetes provider
+  config_path    = local.kubernetes_config_path
+  config_context = local.kubernetes_config_context
+}
+
+provider "helm" {
+  kubernetes {
+    config_path    = local.kubernetes_config_path
+    config_context = local.kubernetes_config_context
+  }
 }
