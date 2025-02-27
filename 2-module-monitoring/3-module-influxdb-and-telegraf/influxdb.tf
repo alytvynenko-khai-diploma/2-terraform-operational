34ac557cbaaaea1969044caae2074f4b959cf418
fecton <bopongamer@gmail.com>
2025-02-27 21:46:36 +0100

    fix(grafana): fix influxdb credentials and connection to grafana

--- 2-module-monitoring/3-module-influxdb-and-telegraf/influxdb.tf
+++ 2-module-monitoring/3-module-influxdb-and-telegraf/influxdb.tf
@@ -6,4 +6,14 @@ resource "helm_release" "influxdb" {
   namespace  = var.namespace
 
   values = [file("${path.module}/influxdb-values.yaml")]
+
+  set {
+    name = "adminUser.password"
+    value = "admin-initial-password" # TODO: replace the password
+  }
+
+  set {
+    name = "adminUser.token"
+    value = "L6CjnlnrSO1wmcP9biNrpp7VdrED6xQ0" # TODO: replace the token by a senvisitive secret value
+  }
 }
