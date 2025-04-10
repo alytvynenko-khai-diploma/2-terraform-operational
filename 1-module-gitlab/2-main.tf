9f4fb07ea6edd9bfcc5636ad8b4d8242a364eb4f
fecton <bopongamer@gmail.com>
2025-04-11 00:03:04 +0200

    feat(gitlab): add comments

--- 1-module-gitlab/2-main.tf
+++ 1-module-gitlab/2-main.tf
@@ -1,21 +1,21 @@
-# resource "helm_release" "gitlab" {
-#   name       = "gitlab"
-#   repository = "https://charts.gitlab.io"
-#   chart      = "gitlab"
-#   version    = "8.8.2"
-#   namespace  = var.namespace
+resource "helm_release" "gitlab" {
+  name       = "gitlab"
+  repository = "https://charts.gitlab.io"
+  chart      = "gitlab"
+  version    = "8.8.2"
+  namespace  = var.namespace
 
-#   values = [file("${path.module}/gitlab-values.yaml")]
+  values = [file("${path.module}/gitlab-values.yaml")]
 
-#   set {
-#     name  = "global.hosts.domain"
-#     value = "ci.alytvynenko.net"
-#   }
-# }
+  set {
+    name  = "global.hosts.domain"
+    value = "ci.alytvynenko.net"
+  }
+}
 
 # FIXME: enable later
+# Should be deployed after helm_release and setting API token
 # module "gitlab-configuration" {
-#   depends_on = [ helm_release.gitlab ]
 #   source = "./1-gitlab-configuration"
 #   namespace = var.namespace
 # }
