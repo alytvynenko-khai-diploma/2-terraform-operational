e697ee07a04a20fbc799fa3ece371bba7621a809
fecton <bopongamer@gmail.com>
2025-04-10 20:04:56 +0200

    refactor: reafactor gitlab module for zero deployment

--- 1-module-gitlab/1-gitlab-configuration/gitlab-groups.tf
+++ 1-module-gitlab/1-gitlab-configuration/gitlab-groups.tf
@@ -26,14 +26,14 @@ resource "gitlab_group" "devops-only" {
 #   description = "[TF MANAGED] The GitLab group created only for Developers and developer purposes"
 # }
 
-resource "gitlab_group_variable" "influxdb-token" {
-  for_each = local.group_variable_list
+# resource "gitlab_group_variable" "influxdb-token" {
+#   for_each = local.group_variable_list
 
-  key = each.key
-  value = each.value
+#   key = each.key
+#   value = each.value
 
-  group = gitlab_group.devops-only.id
-  masked = false
-  hidden = false # TODO: enable it later
-  environment_scope = "*"
-}
+#   group = gitlab_group.devops-only.id
+#   masked = false
+#   hidden = false # TODO: enable it later
+#   environment_scope = "*"
+# }
