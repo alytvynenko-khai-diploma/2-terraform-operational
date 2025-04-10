b91fc25bba2020fa49f2d89363b86a6502d80e0a
fecton <bopongamer@gmail.com>
2025-04-10 20:06:34 +0200

    feat: add google provider to deploy to GKE

--- 1-providers.tf
+++ 1-providers.tf
@@ -20,16 +20,62 @@ terraform {
 locals {
   kubernetes_config_path    = "~/.kube/config" # TODO: set up kubernetes provider
   kubernetes_config_context = "docker-desktop" # TODO: set up kubernetes provider
+
+  project_id = "chrome-lane-454119-a6"
+  region  = "europe-north2" # The cheapest region: https://cloudprice.net/gcp/regions
+  zones    = ["${local.region}-a"]
+  cluster_name = "gke-main"
+
+  data_host = "https://${data.google_container_cluster.main.endpoint}"
+  data_token = data.google_client_config.default.access_token
+  data_cluster_ca_certificate = base64decode(data.google_container_cluster.main.master_auth[0].cluster_ca_certificate)
 }
 
+data "google_client_config" "default" {}
+
+data "google_container_cluster" "main" {
+  name     = local.cluster_name
+  location = local.region
+}
+
+provider "google" {
+  credentials = "${file("account.json")}"
+  project     = local.project_id
+  region      = local.region
+  zone        = local.zones[0]
+}
+
+# ============================
+#        Remote testing
+# ============================
 provider "kubernetes" {
-  config_path    = local.kubernetes_config_path
-  config_context = local.kubernetes_config_context
+  host                   = local.data_host
+  token                  = local.data_token
+  cluster_ca_certificate = local.data_cluster_ca_certificate
 }
 
 provider "helm" {
+  # Fixed according to: https://github.com/hashicorp/terraform-provider-helm/issues/630#issuecomment-996682323
+  repository_config_path = "${path.module}/.helm/repositories.yaml"
+  repository_cache = "${path.module}/.helm"
   kubernetes {
-    config_path    = local.kubernetes_config_path
-    config_context = local.kubernetes_config_context
+    host                   = local.data_host
+    token                  = local.data_token
+    cluster_ca_certificate = local.data_cluster_ca_certificate
   }
 }
+
+# ============================
+#        Local testing
+# ============================
+# provider "kubernetes" {
+#   config_path    = local.kubernetes_config_path
+#   config_context = local.kubernetes_config_context
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     config_path    = local.kubernetes_config_path
+#     config_context = local.kubernetes_config_context
+#   }
+# }
