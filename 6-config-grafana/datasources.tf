edeac8ecb3f25108f698552edc6e85725c34d2fb
fecton <bopongamer@gmail.com>
2025-04-19 21:20:06 +0200

    refactor: remove not needed data source (added automatically)

--- 6-config-grafana/datasources.tf
+++ 6-config-grafana/datasources.tf
@@ -1,9 +1,3 @@
-resource "grafana_data_source" "prometheus" {
-  type = "prometheus"
-  name = "prometheus-server"
-  url  = "http://prometheus-server.infrastructure.svc.cluster.local:80"
-}
-
 resource "grafana_data_source" "influxdb" {
   type = "influxdb"
   name = "influxdb"
