138aee8918a7dab2651e6250d575e5d1d72e0644
fecton <bopongamer@gmail.com>
2025-04-19 21:06:18 +0200

    feat: disable namecheap usage

--- 2-main.tf
+++ 2-main.tf
@@ -21,12 +21,13 @@ module "module-networking" {
 }
 
 # External DNS service
-module "external-namecheap" {
-  source = "./4-external-namecheap"
+# FIXME: enable me only for GCP deployment
+# module "external-namecheap" {
+#   source = "./4-external-namecheap"
 
-  load_balancer_ip = "34.46.38.218" # TODO: implement workaround to retrieve the value automatically
+#   load_balancer_ip = "34.46.38.218" # TODO: implement workaround to retrieve the value automatically
 
-  secret_namecheap_api_user  = var.secret_namecheap_api_user
-  secret_namecheap_api_key   = var.secret_namecheap_api_key
-  secret_namecheap_client_ip = var.secret_namecheap_client_ip
-}
+#   secret_namecheap_api_user  = var.secret_namecheap_api_user
+#   secret_namecheap_api_key   = var.secret_namecheap_api_key
+#   secret_namecheap_client_ip = var.secret_namecheap_client_ip
+# }
