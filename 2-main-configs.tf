7cf532ce2cc8d77294f36a6c0c5630b9464d68ab
fecton <bopongamer@gmail.com>
2025-04-19 21:22:01 +0200

    refactor: disable config grafana

--- 2-main-configs.tf
+++ 2-main-configs.tf
@@ -2,14 +2,13 @@
 //    Configuration section
 // ============================
 # FIXME: enable later
-# module "config-gitlab" {
-#   source = "./5-config-gitlab"
+module "config-gitlab" {
+  source = "./5-config-gitlab"
+  namespace = local.infrastructure_namespace
+}
 
-#   namespace = local.infrastructure_namespace
-# }
-
+# FIXME: disabled because nothing is used
 # module "config-grafana" {
 #   source = "./6-config-grafana"
-
 #   namespace = local.infrastructure_namespace
 # }
