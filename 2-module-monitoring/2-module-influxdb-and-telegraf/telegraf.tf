955db8f7f3f26301213fcfdd82be4fc5d87d2b2d
fecton <bopongamer@gmail.com>
2025-04-28 22:04:35 +0200

    refactor: rename files for structurising

--- 2-module-monitoring/2-module-influxdb-and-telegraf/telegraf.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "telegraf" {
-  name       = "telegraf"
-  repository = "https://helm.influxdata.com/"
-  chart      = "telegraf"
-  version    = "1.8.55"
-  namespace  = var.namespace
-
-  values = [file("${path.module}/telegraf-values.yaml")]
-}
