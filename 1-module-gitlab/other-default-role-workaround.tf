24f23fa8385ba998ca0fbd3c777517b428e3292a
fecton <bopongamer@gmail.com>
2025-05-03 05:25:58 +0200

    refactor: update comment for workaround

--- 1-module-gitlab/other-default-role-workaround.tf
+++ 1-module-gitlab/other-default-role-workaround.tf
@@ -1,5 +1,6 @@
 # FIXME: default role shouldn't have the admin permissions
 # should be fixed in security topic later
+# At the moment, it is needed for gitlab-agent to work. It was migrated to 3-terraform-configurational
 resource "kubernetes_cluster_role_binding" "gitlab_agent_cluster_role_binding" {
   metadata {
     name = "gitlab-agent-cluster-role-binding"
