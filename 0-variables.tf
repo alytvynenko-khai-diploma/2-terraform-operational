a104e0f7e1dddd981e1201a0d592767a59dd1b9d
fecton <bopongamer@gmail.com>
2025-05-18 13:34:34 +0200

    feat: add new variable for namecheap module

--- 0-variables.tf
+++ 0-variables.tf
@@ -1,4 +1,4 @@
-# ./3-external-namecheap
+variable "namecheap_loadbalancer_ip" {}
 variable "secret_namecheap_api_user" {}
 variable "secret_namecheap_api_key" {}
 variable "secret_namecheap_client_ip" {}
